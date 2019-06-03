7ad8220af30c9d378238976d090a293322fccad3
omoikane75 <omoikane75@gmail.com>
2019-06-03 17:08:44 +0900

    add implementations

---
+++ xmltool/cli.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Main CLI Setup and Entrypoint."""
+
+# BSD 2-Clause License
+#
+# Copyright (c) 2019, Yasuhiro Okuno (Koma)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from __future__ import absolute_import, division, print_function
+
+# Import the main click library
+import click
+# Import the sub-command implementations
+from xmltool.schema import schema
+# Import the version information
+from xmltool.version import __version__
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version=__version__)
+def cli():
+    """XML cli tool"""
+    pass
+
+@cli.command()
+@click.argument('SCHEMA')
+@click.argument('XML')
+@click.option(
+    '--verbose', '-v', count=True,
+    help='output in verbose mode'
+    )
+def chk(**kwargs):
+    """Validate XML against SCHEMA file"""
+    schema.validate(kwargs)
+    pass
+
+@cli.command()
+@click.argument('SCHEMA')
+@click.argument('XML')
+@click.argument('JSON')
+@click.option(
+    '--verbose', '-v', count=True,
+    help='output in verbose mode'
+    )
+def json(**kwargs):
+    """Convert XML to JSON using Schema"""
+    schema.toJson(kwargs)
+    pass
+
+
+# Entry point
+def main():
+    """Main script."""
+    cli()
+
+if __name__ == '__main__':
+    main()
---
+++ xmltool/schema/schema.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""exif sort implementation"""
+
+# BSD 2-Clause License
+#
+# Copyright (c) 2019, Yasuhiro Okuno (Koma)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from enum import IntEnum
+
+import click
+import json
+import xmlschema
+
+class Level(IntEnum):
+    NOTSET = 0
+    DEBUG = 10
+    INFO = 20
+    WARNING = 30
+    ERROR = 40
+    CRITICAL = 50
+
+def pout(msg=None, verbose=False, level=Level.INFO, newline=True):
+    """stdout support method
+
+    Keyword Arguments:
+        msg {string} -- message to print (default: {None})
+        verbose {bool} -- Set True to print DEBUG message (default: {False})
+        level {Level} -- Set message level for coloring (default: {Level.INFO})
+        newline {bool} -- set to False if trailing new line is not needed (default: {True})
+    """
+    error=False
+    if level in {Level.NOTSET, Level.DEBUG}:
+        # DEBUG messages
+        if verbose < 2: # should print if larger than 2 -vv
+            return
+        fg = 'magenta'
+    elif level == Level.INFO:
+        fg = 'green' # always print
+    elif level == Level.WARNING:
+        if verbose < 1: # should print if larger than 1 -v
+            return
+        fg = 'yellow'
+        error=True
+    elif level in {Level.ERROR, Level.CRITICAL}:
+        fg = 'red'
+        error=True
+    else:
+        pass
+    click.echo(click.style(str(msg), fg=fg), nl=newline, err=error)
+
+def validate(kwargs):
+    """validate xml against xsd schema file"""
+    rt = True
+    pout(kwargs, kwargs["verbose"], Level.DEBUG)
+    try:
+        pout("Schema: {file}".format(file=kwargs["schema"]), kwargs["verbose"], Level.DEBUG)
+        mySchema = xmlschema.XMLSchema(kwargs["schema"])
+        try:
+            pout("XML: {file}".format(file=kwargs["xml"]), kwargs["verbose"], Level.DEBUG)
+            mySchema.validate(kwargs["xml"])
+        except Exception as e:
+            pout("validation failed", kwargs["verbose"], Level.WARNING)
+            pout("{msg}".format(msg=str(e)), kwargs["verbose"], Level.ERROR)
+            rt = False
+    except Exception as e:
+        pout("XML Schema load failed.", kwargs["verbose"], Level.WARNING)
+        pout("{msg}".format(msg=str(e)), kwargs["verbose"], Level.ERROR)
+        rt = False
+    if rt:
+        pout("XML Schema valid!", kwargs["verbose"], Level.INFO)
+    pass
+
+def toJson(kwargs):
+    """export xml file to json"""
+    pout(kwargs, kwargs["verbose"], Level.DEBUG)
+    err = False
+    try:
+        pout("Schema: {file}".format(file=kwargs["schema"]), kwargs["verbose"], Level.DEBUG)
+        mySchema = xmlschema.XMLSchema(kwargs["schema"], validation='lax')
+        try:
+            pout("XML: {file}".format(file=kwargs["xml"]), kwargs["verbose"], Level.DEBUG)
+            xmlJson = json.dumps(mySchema.to_dict(kwargs["xml"], decimal_type=str), indent=4)
+            pout("{out}".format(out=xmlJson), kwargs["verbose"], Level.DEBUG)
+        except Exception as e:
+            pout("validation failed", kwargs["verbose"], Level.WARNING)
+            pout("{msg}".format(msg=str(e)), kwargs["verbose"], Level.ERROR)
+            err = True
+    except Exception as e:
+        pout("XML Schema load failed.", kwargs["verbose"], Level.WARNING)
+        pout("{msg}".format(msg=str(e)), kwargs["verbose"], Level.ERROR)
+        err = True
+    if not err:
+        # no error output to kwargs["json"]
+        try:
+            outfile = open(kwargs["json"], "w")
+            outfile.write("{output}".format(output=xmlJson))
+        except:
+            pout("could not write to file: {fname}".format(fname=kwargs["json"]), kwargs["verbose"], Level.ERROR)
+        pass
+    pass
